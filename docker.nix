49b4d0559c249073bc0c0e7a4e836e8112b4a79f
Martin Beták <matobet@gmail.com>
2021-01-30 02:12:41 +0100

    Nix Cross-Compile to ARM64 GitHub action

--- docker.nix
+++ docker.nix
@@ -1,18 +1,21 @@
 { pkgs ? import <nixpkgs> {}, dyco-mirror-bot ? import ./default.nix {} }:
 
 let
-  dyco-mirror-bot-exe = dyco-mirror-bot.dyco-mirror-bot.components.exes.dyco-mirror-bot-exe;
+  dyco-mirror-bot-exe = dyco-mirror-bot.projectCross.aarch64-multiplatform.hsPkgs.dyco-mirror-bot.components.exes.dyco-mirror-bot-exe;
+
+  pkgsCross = pkgs.pkgsCross.aarch64-multiplatform;
 
   entrypoint = pkgs.writeScript "entrypoint.sh" ''
-  #!${pkgs.stdenv.shell}
+  #!${pkgsCross.stdenv.shell}
   $@
   '';
 in
-pkgs.dockerTools.buildImage {
+pkgsCross.dockerTools.buildImage {
   name = "dyco-mirror-bot";
-  contents = [ pkgs.bashInteractive pkgs.coreutils dyco-mirror-bot-exe pkgs.iana-etc pkgs.cacert ];
+  tag = "latest";
+  contents = [ dyco-mirror-bot-exe pkgsCross.iana-etc pkgsCross.cacert ];
   config = {
     Cmd = [ "${dyco-mirror-bot-exe}/bin/dyco-mirror-bot-exe" ];
-    Entrypoint = [ entrypoint ];
+    # Entrypoint = [ entrypoint ];
   };
 }
